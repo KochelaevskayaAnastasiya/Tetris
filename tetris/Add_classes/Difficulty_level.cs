a81a3dbba4410f02891eaefd7c0f44fb3cbd1c4f
KochelaevskayaAnastasiya <nastya.kochelayevskaya.02@mail.ru>
2023-12-08 14:15:18 +0400

    НИЧИГО#2

---
+++ tetris/Add_classes/Difficulty_level.cs
@@ -0,0 +1,27 @@
+﻿namespace tetris.Add_classes
+{
+    public class Difficulty_level
+    {
+        private Glass glass;
+        private List<Figure> figures;
+        private double speed;
+        private int points_row;
+        private int points_next_level;
+
+        public Difficulty_level(Glass glass, List<Figure> figures, double speed, int points_row, int points_next_level)
+        {
+            this.glass = glass;
+            this.figures = figures;
+            this.speed = speed;
+            this.points_row = points_row;
+            this.points_next_level = points_next_level;
+        }
+
+        public Glass Glass { get { return glass; } }
+        public List<Figure> Falls { get { return figures; } }
+        public double Speed { get { return speed; } }
+        public int Points_row { get { return points_row; } }
+        public int Players_next_level { get { return points_next_level; } }
+
+    }
+}
---
+++ tetris/Add_classes/Glass.cs
@@ -0,0 +1,17 @@
+﻿namespace tetris.Add_classes
+{
+    public class Glass
+    {
+        private int wigth;
+        private int height;
+
+        public Glass(int w, int h)
+        {
+            wigth = w;
+            height = h;
+        }
+
+        public int Wigth(){return wigth;}
+        public int Height() {return height;}
+    }
+}
