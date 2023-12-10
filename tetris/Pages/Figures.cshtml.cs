d525e69cdf083a5100d176633274f4450fd756d4
KochelaevskayaAnastasiya <nastya.kochelayevskaya.02@mail.ru>
2023-12-10 08:14:39 +0400

    Фигуры без проверки

--- tetris/Pages/Figures.cshtml.cs
+++ tetris/Pages/Figures.cshtml.cs
@@ -1,7 +1,11 @@
+using Microsoft.AspNetCore.DataProtection.KeyManagement.Internal;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Linq;
 using tetris.Add_classes;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace tetris.Pages
 {
@@ -45,6 +49,79 @@ namespace tetris.Pages
                 fig_str.Add(str);
             }
 
+        }
+        public bool CheckFigure(string fi)
+        {
+            /*for(int k=0;k< fig_str.Count; k++)
+            {
+                string queryString = "SELECT * FROM Figures WHERE Structure ='" + fi + "';";
+
+                SqlCommand command = new SqlCommand(queryString, database.getConnection());
+                database.openConnection();
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    reader.Close();
+                    database.closeConnection();
+                }
+                else
+                {
+                    reader.Close();
+                    string sqlExpression = "INSERT INTO Users VALUES ('" + login + "', '" + password + "')";
+                    SqlCommand command_insert = new SqlCommand(sqlExpression, database.getConnection());
+                    int number = command_insert.ExecuteNonQuery();
+                    Console.WriteLine("Добавлен пользователь: {0}", number);
+                    database.closeConnection();
+                }
+            }*/
+            return true;
+        }
+        public int CountFiguBD()
+        {
+            string queryString = "SELECT COUNT(*) FROM [Figures];";
+
+            SqlCommand command = new SqlCommand(queryString, database.getConnection());
+            database.openConnection();
+            SqlDataReader reader = command.ExecuteReader();
+            int l=0;
+            while (reader.Read())
+            {
+                l = int.Parse(reader[0].ToString());
+            }
+            reader.Close();
+            database.closeConnection();
+            return l;
+        }
+            [HttpPost]
+        public IActionResult OnPost()
+        {
+            string s = Request.Form["kkk"];
+            string[] figures_new = s.Split(',');
+            int k = 0;
+            int coun = CountFiguBD();
+            for (int i=0;i<coun;i++)
+            {
+                k = i + 1;
+                string queryString = "UPDATE Figures SET Structure = '"+figures_new[i]+ "' WHERE ID_figure = '" + k + "';";
+                database.openConnection();
+                SqlCommand command_insert = new SqlCommand(queryString, database.getConnection());
+                int number = command_insert.ExecuteNonQuery();
+                Console.WriteLine("Изменено: {0}", number);
+                database.closeConnection();
+            }
+
+            for(int j = coun; j < figures_new.Length; j++) 
+            {
+                string queryString = "INSERT INTO[Figures] VALUES('" + figures_new[j] + "');";
+                database.openConnection();
+                SqlCommand command_insert = new SqlCommand(queryString, database.getConnection());
+                int number = command_insert.ExecuteNonQuery();
+                Console.WriteLine("Добавлено: {0}", number);
+                database.closeConnection();
+            }
+
+            Console.WriteLine();
+            return RedirectToPage("Figures");
         }
     }
 }
