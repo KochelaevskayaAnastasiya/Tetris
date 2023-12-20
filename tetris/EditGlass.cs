fc259ec9595c0aa092b7d585e657a80b2edee097
anastasiyatimonina <nastyatimonina0@yandex.ru>
2023-12-20 16:26:53 +0400

    .

--- tetris/EditGlass.cs
+++ tetris/EditGlass.cs
@@ -4,10 +4,12 @@ namespace tetris
 {
 	public class EditGlass
 	{
-        [Required]
+        [Required (ErrorMessage ="Необходимо заполнить поле \"Высота\"")]
+        [Range(20,40, ErrorMessage = "Значение поля \"Высота\" должно находиться в диапозоне от {1} до {2}")]
         public string Height { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Необходимо заполнить поле \"Ширина\"")]
+        [Range(10, 20, ErrorMessage = "Значение поля \"Высота\" должно находиться в диапозоне от {1} до {2}")]
         public string Width { get; set; }
         
 
