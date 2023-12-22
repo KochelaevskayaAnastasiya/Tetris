ff6bb64a9a081a1b178e301f9be3f24c11e3be0a
anastasiyatimonina <nastyatimonina0@yandex.ru>
2023-12-22 11:51:03 +0400

    .

---
+++ tetris/Pages/AboutSystem2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace tetris.Pages
+{
+    public class AboutSystem2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
